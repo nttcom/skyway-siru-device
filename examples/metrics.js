f53b7f3a6bf64319141b0d526b0337702700f265
kensaku komatsu <kensaku.komatsu@gmail.com>
2017-10-20 22:49:00 +0000

    remove metrics.csv and do not show unneeded log

--- examples/metrics.js
+++ examples/metrics.js
@@ -6,24 +6,13 @@ const yaml = require('js-yaml')
 
 const conf = yaml.safeLoad(fs.readFileSync(__dirname + '/metrics.yaml'))
 const device = new SiRuDevice(conf.roomname, {ssgaddress: conf.ssgaddress})
-const getMetrics = require('./get_metrics')
 
 
 const device_name = 'yourdevice'
-
 const TEMP_FILE = '/sys/class/thermal/thermal_zone0/temp'
 
 device.on('connect', () => {
-  device.get('/metrics/:hour', (req, res) => {
-    getMetrics(req.params.hour)
-      .then(metrics => {
-        res.send(metrics)}
-      )
-      .catch(err => {
-        res.setStatus(403).send(err.toString())
-        console.warn(err)
-      })
-  })
+  device.get('/echo/:mesg', (req, res) => res.send(req.params.mesg))
 
   startSendMetrics()
   setTerminate()
@@ -61,65 +50,16 @@ function startSendMetrics() {
       device.publish( device_name + '/memory', mem_usage )
       device.publish( device_name + '/cpu', cpu_usage )
 
-      storeBucket( device_name + '/memory'.replace('/', '_'), mem_usage)
-      storeBucket( device_name + '/cpu'.replace('/', '_'), cpu_usage)
-
       // publish system temperature
       fs.readFile(TEMP_FILE, (err, data) => {
         if(err) {
-          console.warn(err.toString());
         } else {
           const temperature = JSON.stringify({
             "cpu": parseInt(data) / 1000
           })
           device.publish( device_name + '/temperature', temperature );
-          storeBucket( device_name + '/temperature'.replace('/', '_'), temperature)
         }
       })
    })
 }
 
-
-const buckets = {}
-const INTERVAL = 60
-const FILENAME = 'metrics.csv'
-
-function storeBucket( topic, json_str ) {
-  try {
-    const json = JSON.parse(json_str)
-
-    if(!buckets[topic]) buckets[topic] = {} // initialize for the topic
-
-    const bucket = buckets[topic]
-
-    for(var key in json) {
-      if(!bucket[key]) bucket[key] = []  // initalize for the property
-      bucket[key].push(json[key])
-      if(bucket[key].length > INTERVAL) bucket[key].shift()
-    }
-  } catch(err) {
-    console.warn(err)
-  }
-}
-
-
-
-Rx.Observable.interval(INTERVAL * 1000)
-  .subscribe(() => {
-    const avg = {}
-
-    for(var key in buckets) {
-      var bucket = buckets[key]
-      avg[key] = {}
-      for(var prop in bucket) {
-        var len = bucket[prop].length
-        var sum = 0;
-        for( var i = 0; i < len; i++) sum = sum + bucket[prop][i]
-        var _avg = sum / len
-
-        avg[key][prop] = _avg
-      }
-    }
-    const out = `${Date.now()},${JSON.stringify(avg)}\n`
-    fs.writeFile(FILENAME, out, {flag:'a'})
-  })
